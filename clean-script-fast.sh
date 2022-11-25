f4eea5522379a278e87dbc70c107a967a04c2390
Kusanagi8200 <elijahkaminski230@gmail.com>
2022-11-25 13:33:06 +0100

    + Log Update File

--- clean-script-fast.sh
+++ clean-script-fast.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
+echo #
 echo -e "\033[43;30mSCRIPT DE NETTOYAGE ET DE MISE A JOUR SYSTEME LINUX / Copyright (C) Elijah Kaminski/2022\033[0m"
 
+#Fonction qui vérifie que le script est lancé en sudo ou root.
+
+echo #
 if [ `whoami` != "root" ]
 then
         echo -e "\033[5;41;30mATTENTION. VOUS DEVEZ AVOIR LES DROITS SUDO POUR LANCER CE SCRIPT \033[0m"
         exit 1
 fi
+
+#Fonction qui vérifie la presence des fichiers de log et les crée au besoin. 
+
+echo #
+echo -e "\033[43;30m ---> CHECK DES FICHIERS DE LOG \033[0m"
+
+echo #
+if [ -e /var/log/update_upgrade.log ]
+then
+    echo -e "\033[47;32mFICHIER\033[0m" /var/log/update_upgrade.log = "\033[47;32m OK \033[0m"
+else
+    echo -e "\033[47;31mLE FICHIER N'EXISTE PAS \033[0m" & touch /var/log/update_upgrade.log
+    echo -e "\033[41;33m--> CRÉATION DU FICHIER\033[0m" /var/log/update_upgrade.log = "\033[47;32m DONE \033[0m"
+fi
+
+echo #
+if [ -e /var/log/update_upgrade.err ]
+then
+    echo -e "\033[47;32mFICHIER\033[0m" /var/log/update_upgrade.err = "\033[47;32m OK \033[0m"
+else
+    echo -e "\033[47;31mLE FICHIER N'EXISTE PAS \033[0m" & touch /var/log/update_upgrade.err
+    echo -e "\033[41;33m--> CRÉATION DU FICHIER\033[0m" /var/log/update_upgrade.err = "\033[47;32m DONE \033[0m"
+fi
+echo #
+
+#Séquence de nettoyage systeme update
 
 echo #
 echo -e "\033[43;30m ---> NETTOYAGE PRE MAJ \033[0m"
@@ -35,6 +65,8 @@ echo -e "\033[43;30m <--- FIN DU NETTOYAGE PRE MAJ \033[0m"
 echo #
 echo # 
 
+#Séquence de mise à jours des paquets
+
 echo -e "\033[43;30m ---> MISE A JOUR DES PAQUETS \033[0m"
 apt update && apt list --upgradable
 
@@ -47,6 +79,8 @@ echo #
 echo -e "\033[43;30m <--- MISE À JOUR DES PAQUETS TERMINÉE \033[0m"
 echo #
 echo # 
+
+#Séquence de nettoyage systeme post mise à jour 
 
 echo -e "\033[43;30m ---> NETTOYAGE POST MAJ \033[0m"
 echo #
@@ -86,9 +120,20 @@ echo #
 
 echo -e "\033[43;30m <--- FIN DU NETTOYAGE POST MAJ \033[0m"
 echo #
+
+#Fonction qui verifie les fichiers de log et affiche le log erreur si des erreurs sont presentes.
+
 echo # 
+echo -e "\033[47;31m ---> FICHIER DE LOG ERREUR \033[0m"
 
-
+if [ -N /var/log/update_upgrade.err ]
+  then 
+	echo "ATTENTION - ERREURS LORS DE LA MISE A JOUR" & cat /var/log/update_upgrade.err
+  else
+	echo "Pas d'erreur de mise à jour." 
+fi 
+echo #
+ 
 zenity --title "REBOOT" --question --text "Voulez vous Redémarrer ?"
 if [ $? -eq 0 ]
 then 
@@ -96,3 +141,4 @@ then
 fi
 
 echo -e "\033[43;30m ---> THE END <--- \033[0m"
+echo #
