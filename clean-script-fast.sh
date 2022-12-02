f9e20257f687d4002076baa8da67e51b2362bf90
Kusanagi8200 <elijahkaminski230@gmail.com>
2022-12-02 12:39:35 +0100

    + Log Update + Log Error Update

--- clean-script-fast.sh
+++ clean-script-fast.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo #
-echo -e "\033[43;30mSCRIPT DE NETTOYAGE ET DE MISE A JOUR SYSTEME LINUX / Copyright (C) Elijah Kaminski/2022\033[0m"
+echo -e "\033[43;30m SCRIPT DE NETTOYAGE ET DE MISE A JOUR SYSTEME LINUX / Copyright (C) Elijah Kaminski/2022 \033[0m"
 
 #Fonction qui vérifie que le script est lancé en sudo ou root.
 
 echo #
 if [ `whoami` != "root" ]
 then
-        echo -e "\033[5;41;30mATTENTION. VOUS DEVEZ AVOIR LES DROITS SUDO POUR LANCER CE SCRIPT \033[0m"
+        echo -e "\033[5;41;30m ATTENTION. VOUS DEVEZ AVOIR LES DROITS SUDO POUR LANCER CE SCRIPT \033[0m"
         exit 1
 fi
 
@@ -20,19 +20,19 @@ echo -e "\033[43;30m ---> CHECK DES FICHIERS DE LOG \033[0m"
 echo #
 if [ -e /var/log/update_upgrade.log ]
 then
-    echo -e "\033[47;32mFICHIER\033[0m" /var/log/update_upgrade.log = "\033[47;32m OK \033[0m"
+    echo -e "\033[47;32m FICHIER \033[0m" /var/log/update_upgrade.log = "\033[47;32m OK \033[0m"
 else
-    echo -e "\033[47;31mLE FICHIER N'EXISTE PAS \033[0m" & touch /var/log/update_upgrade.log
-    echo -e "\033[41;33m--> CRÉATION DU FICHIER\033[0m" /var/log/update_upgrade.log = "\033[47;32m DONE \033[0m"
+    echo -e "\033[47;31m LE FICHIER N'EXISTE PAS \033[0m" & touch /var/log/update_upgrade.log
+    echo -e "\033[41;33m --> CRÉATION DU FICHIER \033[0m" /var/log/update_upgrade.log = "\033[47;32m DONE \033[0m"
 fi
 
 echo #
 if [ -e /var/log/update_upgrade.err ]
 then
-    echo -e "\033[47;32mFICHIER\033[0m" /var/log/update_upgrade.err = "\033[47;32m OK \033[0m"
+    echo -e "\033[47;32m FICHIER \033[0m" /var/log/update_upgrade.err = "\033[47;32m OK \033[0m"
 else
-    echo -e "\033[47;31mLE FICHIER N'EXISTE PAS \033[0m" & touch /var/log/update_upgrade.err
-    echo -e "\033[41;33m--> CRÉATION DU FICHIER\033[0m" /var/log/update_upgrade.err = "\033[47;32m DONE \033[0m"
+    echo -e "\033[47;31m LE FICHIER N'EXISTE PAS \033[0m" & touch /var/log/update_upgrade.err
+    echo -e "\033[41;33m --> CRÉATION DU FICHIER \033[0m" /var/log/update_upgrade.err = "\033[47;32m DONE \033[0m"
 fi
 echo #
 
@@ -41,23 +41,23 @@ echo #
 echo #
 echo -e "\033[43;30m ---> NETTOYAGE PRE MAJ \033[0m"
 echo #
-echo -e "\033[44;37mAPT CLEAN\033[0m"
+echo -e "\033[44;37m APT CLEAN \033[0m"
 apt clean 
 echo Done
 echo #
-echo -e "\033[44;37mAPT AUTOCLEAN\033[0m"
+echo -e "\033[44;37m APT AUTOCLEAN \033[0m"
 apt autoclean
 echo #
-echo -e "\033[44;37mAPT REMOVE\033[0m"
+echo -e "\033[44;37m APT REMOVE \033[0m"
 apt remove 
 echo #
-echo -e "\033[44;37mAPT AUTOREMOVE\033[0m"
+echo -e "\033[44;37m APT AUTOREMOVE \033[0m"
 apt autoremove
 echo #
-echo -e "\033[44;37mAPT PURGE\033[0m"
+echo -e "\033[44;37m APT PURGE \033[0m"
 apt purge
 echo #
-echo -e "\033[44;37mAPT CHECK\033[0m"
+echo -e "\033[44;37m APT CHECK \033[0m"
 apt check
 echo #
 echo -e "\033[43;30m <--- FIN DU NETTOYAGE PRE MAJ \033[0m"
@@ -84,38 +84,38 @@ echo #
 
 echo -e "\033[43;30m ---> NETTOYAGE POST MAJ \033[0m"
 echo #
-echo -e "\033[44;37mAPT CLEAN\033[0m"
+echo -e "\033[44;37m APT CLEAN \033[0m"
 apt clean 
 echo Done
 echo #
-echo -e "\033[44;37mAPT AUTOCLEAN\033[0m"
+echo -e "\033[44;37m APT AUTOCLEAN \033[0m"
 apt autoclean
 echo #
-echo -e "\033[44;37mAPT REMOVE\033[0m"
+echo -e "\033[44;37m APT REMOVE \033[0m"
 apt remove 
 echo #
-echo -e "\033[44;37mAPT AUTOREMOVE\033[0m"
+echo -e "\033[44;37m APT AUTOREMOVE \033[0m"
 apt autoremove
 echo #
-echo -e "\033[44;37mAPT PURGE\033[0m"
+echo -e "\033[44;37m APT PURGE \033[0m"
 apt purge
 echo #
-echo -e "\033[44;37mAPT CHECK\033[0m"
+echo -e "\033[44;37m APT CHECK \033[0m"
 apt check
 echo #
 
 echo -e "\033[43;30m ---> NETTOYAGE DU CACHE \033[0m"
 sync; echo 3 > /proc/sys/vm/drop_caches
-echo Done
+echo -e "\033[44;37m DONE \033[0m"
 echo # 
 
 echo -e "\033[43;30m ---> NETTOYAGE POUBELLE \033[0m"
 rm -r -f ~/.local/share/Trash/files/*
-echo Done
+echo -e "\033[44;37m DONE \033[0m"
 echo #
 
 echo -e "\033[43;30m ---> NETTOYAGE DES CONFIG DE PAQUETS \033[0m"
-[[ $(dpkg -l | grep ^rc) ]] && sudo dpkg -P $(dpkg -l | awk '/^rc/{print $2}') || echo "Pas de paquet à purger."
+[[ $(dpkg -l | grep ^rc) ]] && sudo dpkg -P $(dpkg -l | awk '/^rc/{print $2}') || echo -e "\033[44;37m PAS DE PAQUETS À PURGER \033[0m"
 echo #
 
 echo -e "\033[43;30m <--- FIN DU NETTOYAGE POST MAJ \033[0m"
@@ -124,17 +124,18 @@ echo #
 #Fonction qui verifie les fichiers de log et affiche le log erreur si des erreurs sont presentes.
 
 echo # 
-echo -e "\033[47;31m ---> FICHIER DE LOG ERREUR \033[0m"
+echo -e "\033[43;30m ---> FICHIER LOG ERREUR MAJ \033[0m"
 
 if [ -N /var/log/update_upgrade.err ]
   then 
-	echo "ATTENTION - ERREURS LORS DE LA MISE A JOUR" & cat /var/log/update_upgrade.err
-  else
-	echo "Pas d'erreur de mise à jour." 
+	echo -e "\033[5;41;37m ATTENTION \033[0m" & cat /var/log/update_upgrade.err 
+        echo #
+else
+	echo -e "\033[44;37m PAS D'ERREUR DE MAJ \033[0m" 
 fi 
 echo #
- 
-zenity --title "REBOOT" --question --text "Voulez vous Redémarrer ?"
+
+zenity --title "REBOOT" --question --text "VOULEZ VOUS REDÉMARRER ?"
 if [ $? -eq 0 ]
 then 
         reboot
